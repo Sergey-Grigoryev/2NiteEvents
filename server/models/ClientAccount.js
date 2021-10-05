d50144c6d8c6909af2bde96082def6a4b6203577
Dyravuth Yorn <yorn.dyravuth@gmail.com>
2021-10-04 22:28:37 -0700

    made reference from ClientAccount to Business

--- server/models/ClientAccount.js
+++ server/models/ClientAccount.js
@@ -1,39 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const { Schema } = mongoose;
-const bcrypt = require('bcrypt');
+const { Schema, ObjectId } = mongoose;
+const bcrypt = require("bcrypt");
 
-const clientAccountSchema = new Schema ({
-    id: {
-        type: String,
-        required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    businessAddress: {
-      type: String,
-      required: true,
-    },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 5
-      },
-
+const clientAccountSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  businessId: {
+    type: Schema.Types.ObjectId,
+    ref: "Business",
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
 });
 
 // compare the incoming password with the hashed password
-clientAccountSchema.methods.isCorrectPassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
-  };
+clientAccountSchema.methods.isCorrectPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-const clientAccount = mongoose.model('ClientAccount', clientAccountSchema);
+const clientAccount = mongoose.model("ClientAccount", clientAccountSchema);
 
 module.exports = clientAccount;
